c08aeae328ebe825628b715bccfe1a171ddb18f0
Rodrigo Suarez <ing.rsuarez@gmail.com>
2023-12-08 16:10:51 -0300

    New styles on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 
 <x-home>
-    <div class="w-screen relative mt-20 flex items-center justify-center font-sans animate-[fadein_2s_ease-in_1]">
+    <div class="w-screen relative mt-20 flex items-center justify-center font-display font-medium tracking-tight animate-[fadein_2s_ease-in_1]">
                 
         {{-- <img class="rounded-md w-full" src="{{asset('img/trabajadores01.jpg')}}" alt=""> --}}
         <div class="relative">
             <img class="w-screen shadow-lg pointer-events-none" src="{{asset('img/01/oilwell.jpg')}}" alt="">
-            <div class="lg:absolute relative cursor-pointer lg:top-40 lg:left-20 px-4 py-3 lg:w-80  rounded-lg">
-                <h1 class="text-gray-100 font-mono font-bold text-8xl select-none  hover:text-orange-300">NQN PETROL
+            <div class="lg:absolute relative cursor-pointer lg:top-40 lg:left-20 px-4 py-3 lg:w-1/2 bg-slate-600 bg-opacity-50 rounded-lg">
+                <h1 class="text-gray-100 font-display font-medium tracking-tight text-8xl select-none  hover:text-orange-300">NQN PETROL
                 </h1>
                 <br>
                 <p class="text-gray-400 lg:text-white text-xl shadow-2xl select-none">Desarrollamos soluciones
@@ -25,11 +25,28 @@
     <div class="container bg-gray-200 mx-auto w-full h-full">
         <div class="relative wrap overflow-hidden h-full">
            
-            <div class="relative mb-8 grid grid-cols-1 md:grid-cols-2 bg-white w-full py-10 px-4">
+            {{-- <div class="relative mb-8 grid grid-cols-1 md:grid-cols-2 bg-white w-full py-10 px-4"> --}}
+                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+                    <h1 class="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+                        Nuestros servicios se basan en las 
+                        <!-- --> 
+                        <span class="relative whitespace-nowrap text-blue-600">
+                        <svg aria-hidden="true" viewBox="0 0 418 42" class="absolute left-0 top-2/3 h-[0.58em] w-full fill-blue-300/70" preserveAspectRatio="none">
+                                <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z">
+                            </path>
+                        </svg>
+                        <span class="relative">mejores practicas</span> 
+                        </span>
+                        de la industria para asegurar calidad.
+                    </h1>
+                    <p class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+                        Desde el 2008 atendemos las necesidades de las industria del petróleo y gas a través de la provisión de servicios de logística, equipamiento y personal.
 
                 
-
-            </div>
+                    </p>
+                
+                </div>
+            {{-- </div> --}}
 
             <!-- First item -->
            
