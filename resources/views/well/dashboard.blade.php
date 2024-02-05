b72333798437d3cd19fe3a160fe1db14afe1c89f
Rodrigo Suarez <ing.rsuarez@gmail.com>
2024-02-04 23:16:41 -0300

    well dashboard list

--- resources/views/well/dashboard.blade.php
+++ resources/views/well/dashboard.blade.php
@@ -5,8 +5,9 @@
         <a href="{{route('well.new')}}" class="m-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 
             border-blue-700 hover:border-blue-500 rounded">Nuevo pozo</a>
 
-        <a href="{{route('well.new')}}" class="m-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 
+        <a href="{{route('well.list')}}" class="m-4 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 
             border-blue-700 hover:border-blue-500 rounded">Listado de pozos</a>
+            
     </div>
     
     <script>
